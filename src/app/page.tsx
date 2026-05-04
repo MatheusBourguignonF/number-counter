5051e342408ce6641eab42f33a9558aeee1d5489
MatheusBourguignonF <teusaoboladao@gmail.com>
2026-05-04 20:17:39 -0300

    feat: create number counter with increment and decrement logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,65 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
 
 export default function Home() {
+
+  const [number, setNumber] = useState<number>(0)
+  
+  let property = "Null"
+  let color = "gray"
+
+  if(number > 0){
+    property = "Positive"
+    color = "green"
+  } else if(number < 0){
+    property = "Negative"
+    color = "red"
+  } else{
+    property = "Null"
+    color = "gray"
+  }
+
+  function increment(value:number){
+    setNumber(prev => Math.min(prev + value, 1000))
+  }
+  function reset(){
+    setNumber(0)
+  }
+  function decrement(value:number){
+    setNumber(prev => Math.max(prev - value, -1000))
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    <div>
+      <main className="ml-10 mt-10">
+        <div className="flex flex-col items-center gap-5">
+          <div>
+            
+          </div>
+          <div className="flex flex-col items-center">
+            <h3 className="font-bold">{property}</h3> 
+            <h2 className="text-[25px] font-bold" style={{color}}>{number}</h2>
+            <button onClick={reset} className="bg-black text-white rounded-full w-31 h-7  font-bold">Reset</button>
+          </div>
+          <div className="flex gap-14">
+            <div className="flex flex-col items-center gap-1.5">
+              <h2 className="bg-black text-white rounded-full w-30 h-7 font-bold flex items-center justify-center">Increment</h2>
+              <button onClick={() => {increment(1)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">+ 1</button>
+              <button onClick={() => {increment(2)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">+ 2</button>
+              <button onClick={() => {increment(5)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">+ 5</button>
+              <button onClick={() => {increment(10)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">+ 10</button>
+              <button onClick={() => {increment(100)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">+ 100</button>
+          </div>
+          <div className="flex flex-col items-center gap-1.5">
+            <h2 className="bg-black text-white rounded-full w-30 h-7 font-bold flex items-center justify-center">Decrement</h2>
+            <button onClick={() => {decrement(1)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">- 1</button>
+            <button onClick={() => {decrement(2)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">- 2</button>
+            <button onClick={() => {decrement(5)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">- 5</button>
+            <button onClick={() => {decrement(10)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">- 10</button>
+            <button onClick={() => {decrement(100)}} className="bg-black text-white rounded-full w-15 h-7 font-bold">- 100</button>
+          </div>
+          </div> 
         </div>
       </main>
     </div>
